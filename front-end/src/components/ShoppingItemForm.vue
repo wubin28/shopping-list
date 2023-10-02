21e17d4b6f699f31f0a6f4787d787e3afacec73d
Ben Wu <wubin28@gmail.com>
2023-10-02 16:10:45 +0800

    feat: add a form with a title input field and an add button in the shopping list page

---
+++ front-end/src/components/ShoppingItemForm.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { ElForm, ElFormItem, ElInput, ElButton } from 'element-plus'
+
+const formInput = ref({ title: '', purchased: false })
+
+const onSubmit = () => {
+  console.log('ShoppingItem', formInput.value)
+}
+</script>
+
+<template>
+  <el-form @submit.prevent :inline="true" :model="formInput" class="shopping-item-form-input">
+    <el-form-item label="Title" prop="title">
+      <el-input v-model="formInput.title" placeholder="Enter Title"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit()">Add</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<style scoped></style>
--- front-end/src/components/ShoppingList.vue
+++ front-end/src/components/ShoppingList.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ShoppingItemForm from '@/components/ShoppingItemForm.vue'
+</script>
 
 <template>
   <h1>ShoppingList</h1>
+  <shopping-item-form></shopping-item-form>
 </template>
 
 <style scoped></style>
