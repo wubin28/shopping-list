24cf6e3be4c8b19f471e6337f65cf45c25f6bd92
Ben Wu <wubin28@gmail.com>
2023-10-02 13:19:40 +0800

    feat: add a button using element plus

--- front-end/src/components/HelloWorld.vue
+++ front-end/src/components/HelloWorld.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ElButton } from 'element-plus'
+
 defineProps<{
   msg: string
 }>()
@@ -7,11 +9,7 @@ defineProps<{
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+    <el-button>Example</el-button>
   </div>
 </template>
 
