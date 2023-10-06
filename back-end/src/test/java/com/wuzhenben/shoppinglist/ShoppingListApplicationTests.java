c76e1a1edb2ba140de1d3d411a437c40bf4c488e
Ben Wu <wubin28@gmail.com>
2023-10-06 23:19:43 +0800

    refactor: rename Shoppinglist to ShoppingList

--- back-end/src/test/java/com/wuzhenben/shoppinglist/ShoppingListApplicationTests.java
+++ back-end/src/test/java/com/wuzhenben/shoppinglist/ShoppingListApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShoppinglistApplicationTests {
+class ShoppingListApplicationTests {
 
 	@Test
 	void contextLoads() {
