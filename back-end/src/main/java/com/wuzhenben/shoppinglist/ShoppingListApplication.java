6bacb4d7e34d7ef1256b9e759a18330d28012712
Ben Wu <wubin28@gmail.com>
2023-10-06 20:34:58 +0800

    refactor: rename Shoppinglist to ShoppingList

--- back-end/src/main/java/com/wuzhenben/shoppinglist/ShoppingListApplication.java
+++ back-end/src/main/java/com/wuzhenben/shoppinglist/ShoppingListApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoppinglistApplication {
+public class ShoppingListApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoppinglistApplication.class, args);
+		SpringApplication.run(ShoppingListApplication.class, args);
 	}
 
 }
