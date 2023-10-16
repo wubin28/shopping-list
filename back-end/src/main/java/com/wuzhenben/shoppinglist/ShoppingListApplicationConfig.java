4d9c433ee101a4e521fd45235b16097cf1ae9961
Ben Wu <wubin28@gmail.com>
2023-10-16 21:17:01 +0800

    feat: deploy shopping list app to azure k8s service. When access the front end, get the following error: Access to XMLHttpRequest at 'http://shopping-list-api-service:8081/api/v1/shopping-items' from origin 'http://20.232.53.195:8080' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.

--- back-end/src/main/java/com/wuzhenben/shoppinglist/ShoppingListApplicationConfig.java
+++ back-end/src/main/java/com/wuzhenben/shoppinglist/ShoppingListApplicationConfig.java
@@ -17,7 +17,7 @@ public class ShoppingListApplicationConfig {
                 registry.addMapping(
                     "/api/v1/shopping-items/**"
                 ).allowedOrigins(
-                    "http://localhost:8080"
+                    "*"
                 ).allowedMethods(
                     "GET", "POST", "PUT", "DELETE");
             }
